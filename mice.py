markov chains are much faster. some numerical instability problems do need to be fixed, but hopefully that can come later

mice.py
@@ -88,7 +88,10 @@ class Maze:
     n = Q.shape[0]
     I = np.identity(n)
     try:
-      return (np.linalg.solve(I - Q, np.ones((n, 1)))).item(0, 0)
+      ev = np.linalg.solve(I - Q, np.ones((n, 1))).item(0, 0)
+      if ev < 0:
+        return np.inf
+      return ev
     except np.linalg.linalg.LinAlgError:
       return np.inf
 